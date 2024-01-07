50ebc7e90ee4392e7689389791a39338c9e7454d
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-01-07 14:33:59 -0800

    init the fixed quantity handler

--- Models/req/inv_activation/activation.js
+++ Models/req/inv_activation/activation.js
@@ -1,41 +1,45 @@
 
-const fixed_look_up = (productType, productSize) => {
-  if(productType == 122){
-    //liquid 
-    if(productSize == 3 || 1){
-      //50 ml fixed num  
-    }
-    if(productSize == 2 || 4) {
-      //30 ml
-    }
-    if(productSize == 9 || 10){
-      //60 ml
-    }
-    else{
-      //fulvic foir example
-      return null;
-    }
+// const fixed_look_up = (productType, productSize) => {
+//   if(productType == 122){
+//     //liquid 
+//     if(productSize == 3 || 1){
+//       //50 ml fixed num  
+//       return 
+//     }
+//     if(productSize == 2 || 4) {
+//       //30 ml
+//       return 
+//     }
+//     if(productSize == 9 || 10){
+//       //60 ml
+//       return 
+//     }
+//     else{
+//       //fulvic foir example
+//       return null;
+//     }
     
-  }
-  if(productType == 44){
-    if(productSize == 5){
-      //use subcomponenttype to get size
-      //return fixed box value
-    }
-    if(productSize == 6)
-    {
-
-    }
-    if(productSize == 7){
-      
-    }
-    else{
-      return null;
-      //matcha similar products will be direct quantity
-    }
-    //pills
-  }
-}
+//   }
+//   if(productType == 44){
+//     if(productSize == 5){
+//       //use subcomponenttype to get size
+//       //return fixed box value
+//       return 
+//     }
+//     if(productSize == 6)
+//     {
+// return
+//     }
+//     if(productSize == 7){
+//       return 
+//     }
+//     else{
+//       return null;
+//       //matcha similar products will be direct quantity
+//     }
+//     //pills
+//   }
+// }
 
 
 class product_inventory {
@@ -46,20 +50,15 @@ class product_inventory {
     this.QUANTITY = parseInt(args.QUANTITY);
     this.MULTIPLIER = args.MULTIPLIER;
     this.EMPLOYEE_NAME = args.EMPLOYEE_NAME;
-    this.PRODUCT_TYPE  = args.PRODUCT_TYPE;
-    this.PRODUCT_FIXED = args.PRODUCT_FIXED;
-    this.quantityHandler();
   }
 
-  quantityHandler(){
-    if(this.PRODUCT_FIXED == true){
-      this.QUANTITY = fixed_look_up(this.PRODUCT_TYPE, this.PRODUCT_SUBCOMP) != null ? fixed_look_up(this.PRODUCT_TYPE, this.PRODUCT_SUBCOMP) : this.QUANTITY;
-    }
+  // quantityHandler(){
+  //   if(this.PRODUCT_FIXED == true){
+  //     this.QUANTITY = fixed_look_up(this.PRODUCT_TYPE, this.PRODUCT_SUBCOMP) != null ? fixed_look_up(this.PRODUCT_TYPE, this.PRODUCT_SUBCOMP) : this.QUANTITY;
+  //   }
+  to_arr() {
+    return [this.PRODUCT_ID, this.QUANTITY, this.EMPLOYEE_ID];
   }
-
-  // to_arr() {
-  //   return [this.PRODUCT_ID, this.QUANTITY, this.EMPLOYEE_ID];
-  // }
 }
 
 const product_inventory_model = (args, callback) => {
