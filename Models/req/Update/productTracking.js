35a1cc26e43972f07c64cd2f15c8be1cccdf9a77
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-01-29 20:41:46 -0800

    fixed tracking schema

--- Models/req/Update/productTracking.js
+++ Models/req/Update/productTracking.js
@@ -1,13 +1,13 @@
 class trackingShema {
-    constructor(args){
-        this.productID = args.productID;
-        this.quantity = args.quantity >= 0 ? args.quantity : null;
-    }
-    to_arr(){
-        return [this.quantity, this.productID];
-    }
+  constructor(args) {
+    this.productID = args.productID;
+    this.quantity = args.quantity ? args.quantity : null;
+  }
+  to_arr() {
+    return [this.quantity, this.productID];
+  }
 }
 
 exports.trackingShema = (args, callback) => {
-    return callback(new trackingShema(args));
-} 
+  return callback(new trackingShema(args));
+};
