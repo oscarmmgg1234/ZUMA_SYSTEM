70d38177248399b3cd0880ff9bdfaacd309b0cae
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-02-01 09:58:53 -0800

    added pm2 server configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "Inventory API",
+      script: "index.js",
+      cron_restart: "0 2 * * *", // Restart at 2:00 AM every day
+      // Other configurations like instances, exec_mode etc.
+    },
+  ],
+};
