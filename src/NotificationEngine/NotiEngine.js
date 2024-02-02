d13064bafef1153d034f36afa0f61596230819f5
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-02-02 13:27:49 -0800

    excluded sat-sun emails

--- src/NotificationEngine/NotiEngine.js
+++ src/NotificationEngine/NotiEngine.js
@@ -1,11 +1,10 @@
 const { emailGeneration } = require("./emailGeneration");
-
 const cron = require("node-cron");
 
 const notification = async () => {
-  // Schedule the task to run every day at 8:00 AM
+  // Schedule the task to run every weekday (Monday to Friday) at 8:00 AM
   cron.schedule(
-    "0 8 * * *",
+    "0 8 * * 1-5", // '1-5' specifies Monday to Friday
     async () => {
       await emailGeneration();
     },
