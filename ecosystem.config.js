2d03c068f0518ce26e9d8fa26462493da8cd94e9
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-02-02 11:17:43 -0800

    pm2 server configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "NotificationService",
+      script: "index.js", // Restart at 2:00 AM every day
+      // Other configurations like instances, exec_mode etc.
+    },
+  ],
+};
