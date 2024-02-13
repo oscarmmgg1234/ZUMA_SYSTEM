d1d6dd3d1edfbf18924718130b80a208a69fba4a
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-02-13 14:02:52 -0800

    modified the scheduler

--- src/NotificationEngine/NotiEngine.js
+++ src/NotificationEngine/NotiEngine.js
@@ -3,17 +3,16 @@ const cron = require("node-cron");
 
 const notification = async () => {
   // Schedule the task to run every weekday (Monday to Friday) at 8:00 AM
-  // cron.schedule(
-  //   "0 8 * * 1-5", // '1-5' specifies Monday to Friday
-  //   async () => {
-  //     await emailGeneration();
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: "America/Los_Angeles", // Replace with your actual timezone
-  //   }
-  // );
-  await emailGeneration();
+  cron.schedule(
+    "0 8 * * 1-5", // '1-5' specifies Monday to Friday
+    async () => {
+      await emailGeneration();
+    },
+    {
+      scheduled: true,
+      timezone: "America/Los_Angeles", // Replace with your actual timezone
+    }
+  );
 };
 
 exports.notification = notification;
