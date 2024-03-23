e257981ab7e0f36d10a5e81b65871d250ecc28bf
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-03-22 17:06:13 -0700

    more production ready for new feautre

--- Sockets/SystemStatusSocket.js
+++ Sockets/SystemStatusSocket.js
@@ -2,26 +2,22 @@ const WebSocket = require("ws");
 const socketPort = 3005;
 
 const init_sock_server = () => {
-const wss = new WebSocket.Server({ port: socketPort }, () =>
-  console.log(`WebSocket server is listening on port ${socketPort}`)
-);
+  const wss = new WebSocket.Server({ port: socketPort }, () =>
+    console.log(`WebSocket server is listening on port ${socketPort}`)
+  );
 
-wss.on("connection", function connection(ws) {
-  // Send a JSON-formatted message to the connected client
-  console.log("Client connected");
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
+  wss.on("connection", function connection(ws) {
+    // Send a JSON-formatted message to the connected client
+    ws.on("message", (message) => {
+      console.log(`Received message: ${message}`);
+    });
+    ws.on("error", (error) => {
+      console.error(`WebSocket error: ${error}`);
+    });
+    // Handle WebSocket connection closing
+    ws.on("close", () => {
+      // Additional server-side handling can go here
+    });
   });
-  ws.on("error", (error) => {
-    console.error(`WebSocket error: ${error}`);
-  });
-  // Handle WebSocket connection closing
-  ws.on("close", () => {
-    console.log("Client disconnected");
-    // Additional server-side handling can go here
-  });
-});
-
-}
+};
 module.exports = init_sock_server;
-
