1b1ec90b8b5ebcb20ab1b6e0c703050f3205a44e
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-04-19 11:40:07 -0700

    progress in implementing function registries

--- Core/Engine/Registry/functionRegistry.js
+++ Core/Engine/Registry/functionRegistry.js
@@ -1,3 +1,14 @@
+/* 
+============================================
+Author: Oscar Maldonado
+Date: 04/18/2024
+Function Registry
+This module is used to store the functions that will be used by the core engine.
+============================================
+*/
+
+const { db_registry } = require("../DBLayer/DAO/registry");
+
 class FunctionRegistry {
   constructor() {}
   getFunction(key, id) {
@@ -19,5 +30,12 @@ class FunctionRegistry {
   shipment_reg = new Map();
   update_reg = new Map();
 }
+
+const registry = new FunctionRegistry();
+//activation registry
+registry.activation_reg.set("00321", (db_handle, args, value, custom) => {
+  //Insert Row for Activation Log
+  db_registry.getFunction("AC", "00321")(db_handle, args, value, custom);
+});
 
 exports.FunctionRegistry = FunctionRegistry;
