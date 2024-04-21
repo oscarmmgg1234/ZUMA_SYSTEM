c6eca4a1029f831cc402e1f51c17d6f51de489e3
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-04-21 13:15:57 -0700

    creating functions for function registry

--- Core/Engine/Registry/functionRegistry.js
+++ Core/Engine/Registry/functionRegistry.js
@@ -8,6 +8,7 @@ This module is used to store the functions that will be used by the core engine.
 */
 
 const { query_manager } = require("../../../DB/query_manager");
+const { util } = require("../../Utility/Constants");
 const knex = query_manager;
 
 class FunctionRegistry {
@@ -28,7 +29,7 @@ class FunctionRegistry {
       console.error("Error during initialization of FunctionRegistry:", error);
     }
   }
-
+  // deangerous way to solve the problem as it can be a security risk but there is no sesative information in program or device this hosted on, but this allow me to dynamically add functions to the registry and they work as expected with minimal back end intervention
   async fetchAndUpdateRegistry(query) {
     try {
       let registry = await knex.raw(query);
@@ -38,16 +39,17 @@ class FunctionRegistry {
         const func = eval(`(${current.protocol})`);
         this.registry_map.set(current.id, func);
       });
+      console.log(this.registry_map.size, "functions in registry.");
     } catch (error) {
       console.error("Error fetching and updating registry:", error);
     }
   }
-  
+
   async getFunction(id) {
     return await this.pollFunction(id);
   }
   // Polling function to check if the function is available in case of calling function before registry class updates map and returns the function
-   async pollFunction(id, attempts = 3) {
+  async pollFunction(id, attempts = 3) {
     return new Promise((resolve, reject) => {
       const attemptFetch = (remainingAttempts) => {
         if (this.registry_map.size > 0) {
@@ -60,12 +62,39 @@ class FunctionRegistry {
         } else if (remainingAttempts > 0) {
           setTimeout(() => attemptFetch(remainingAttempts - 1), 200); // Wait 200ms then retry
         } else {
-          reject(new Error("Function retrieval failed after maximum attempts."));
+          reject(
+            new Error("Function retrieval failed after maximum attempts.")
+          );
         }
       };
       attemptFetch(attempts);
     });
   }
 }
+
+const string = async function insertIntoReductionTableLiquid(
+  db_handle,
+  args,
+  product_id,
+  custom
+) {
+  await db_handle.raw(
+    "INSERT INTO inventory_consumption (PRODUCT_ID, QUANTITY, EMPLOYEE_ID, TRANSACTIONID) VALUES (?, ?, ?, ?)",
+    [
+      product_id,
+      util.productConsumption(args.productBottleSizeML, args.productQuantity, args.productBaseGallon),
+      args.employee_id,
+      args.TRANSACTIONID,
+    ]
+  );
+}
+
+async function InsertInto() {
+  await knex.raw(
+    "INSERT INTO protocol_registry (class, id, protocol, description) VALUES (?, ?, ?, ?)",
+    ["RD", "48fhw", string, "Inserts into Inventory Consumption table"]
+  );
+}
+InsertInto();
 
 module.exports.FunctionRegistry = FunctionRegistry;
--- Core/Utility/Constants.js
+++ Core/Utility/Constants.js
@@ -3,4 +3,45 @@ Author: Oscar Maldonado
 Date: 04/18/2024
 Engine Utility
 */
+const { query_manager } = require("../../../DB/query_manager");
 
+const knex = query_manager;
+
+const productConsumption = (
+  productBottleSizeML = 50,
+  productQuantity,
+  productBaseGallon
+) => {
+  const productBaseGallon_toMill = productBaseGallon * 3785.41;
+  return (productBottleSizeML * productQuantity) / productBaseGallon_toMill;
+};
+
+const glycerinCompsumption = async (
+  productQuantity,
+  productBottleSizeML = 50,
+  product_id,
+  ratio
+) => {
+  const glycerinBottleSize = await knex.raw(
+    "SELECT GlycerinGallonUnitConstant FROM system_config WHERE system_config.Index = 1"
+  );
+
+  const glycerinBottleAmountGALLONS_toMill =
+    glycerinBottleSize[0][0].GlycerinGallonUnitConstant * 3785.41;
+  const productGlycerinAmountOZ_toMill = ratio * 29.5735;
+  const totalMillInMixture =
+    glycerinBottleAmountGALLONS_toMill + productGlycerinAmountOZ_toMill;
+
+  return (
+    (((productBottleSizeML * productQuantity) / totalMillInMixture) *
+      productGlycerinAmountOZ_toMill) /
+    glycerinBottleAmountGALLONS_toMill
+  );
+};
+
+const ultil = {
+  productConsumption: productConsumption,
+  glycerinCompsumption: glycerinCompsumption,
+};
+
+exports.ultil = ultil;
