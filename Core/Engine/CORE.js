3cbbc308d7cc401ae7c17d5a3416764fb1a2acb4
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-04-26 17:31:11 -0700

    added core exec as a wrapper for post processing like barcode generation and result output

---
+++ Core/Engine/CORE.js
@@ -0,0 +1,43 @@
+/*
+author: Oscar Maldonado
+date: 04/18/2024
+Api Engine 2.0
+*/
+
+const { core_engine } = require("./CORE_ENGINE");
+const { init_services } = require("../../Services/Services.js");
+
+const services = init_services();
+
+const core_exec = async (args, barcodeInput) => {
+  try {
+    const coreSuccess = await core_engine(args);
+    if (!coreSuccess) {
+      return { status: "error", message: "Core engine failed" };
+    }
+
+    const success = {
+      status: "success",
+      message: "Transaction completed successfully",
+    };
+
+    if (barcodeInput) {
+      const barcodeData = await new Promise((resolve, reject) => {
+        services.barcode_gen(barcodeInput, (data) => {
+          if (data) {
+            resolve(data);
+          } else {
+            reject("Barcode generation failed");
+          }
+        });
+      });
+      return { ...success, barcodeBuffer: barcodeData };
+    }
+
+    return success;
+  } catch (error) {
+    return { status: "error", message: `Error: ${error.message}` };
+  }
+};
+
+exports.core_exec = core_exec;
--- Core/Engine/CORE_ENGINE.js
+++ Core/Engine/CORE_ENGINE.js
@@ -19,39 +19,31 @@ const core_engine = async (args) => {
     // Getting the transaction object
     db_handle = await transactionUnit();
     try {
-      //linked list of function references
+      //init token parser and retrive the symbol table
       const protocol = symbolTable(args.process_token, FunctionRegistry);
       for (let i = 0; i < protocol.size; i++) {
         let current = protocol.getData();
-        //process
-        /*
-        db_handle: transaction object
-        args: req object for any need of the function
-        current.value: can be the id of product to be referenced by the function
-        current.custom: any additional data needed by the function
-      */
+        //auxiliary object for custom behaivor
         const auxiliary = {
           auxiliaryParam: current.auxiliaryParam,
           nextAuxiliaryParam: current.nextAuxiliaryParam,
           lastAuxiliaryParam: current.lastAuxiliaryParam,
         };
+        //execute the function
         await current.proto(db_handle, args.args, current.value, auxiliary);
         protocol.next();
       }
-      // Placeholder for database operations
-      // Example: await db_handle.raw('YOUR SQL QUERY HERE');
 
-      // If operations are successful, commit the transaction
       await db_handle.commit();
+      return true;
     } catch (error) {
       throw error;
     }
   } catch (error) {
-    // On error, check if db_handle exists and rollback
     if (db_handle) {
       await db_handle.rollback();
     }
-    console.error("Transaction failed:", error);
+    return false;
   }
 };
 
