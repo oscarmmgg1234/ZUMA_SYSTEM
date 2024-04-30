43a8540221e44621cda99cdf250ec8196c6d0331
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-04-29 21:09:50 -0700

    created production and development flow as well as started db transition

--- Helpers/EngineComponents/transactionErrorHandler.js
+++
@@ -1,9 +0,0 @@
-exports.TransactionHandler = class transactionErrorHandler {
-  constructor() {}
-  sucessHandler() {
-    return { status: true, message: "Transaction Successful" };
-  }
-  errorHandler(mess) {
-    return { status: false, message: mess };
-  }
-};
--- Helpers/helper_interface.js
+++ Helpers/helper_interface.js
@@ -1,18 +1,8 @@
-const { activation_engine } = require("./activation_engine");
-const { reduction_engine } = require("./reduction_inv");
-const { shipment_engine } = require("./shipment_engine");
+
 const { transaction_engine } = require("./transaction_engine");
 
 class helper {
-  activation_engine(args, callback) {
-    activation_engine(args, callback);
-  }
-  reduction_engine(args) {
-    reduction_engine(args);
-  }
-  shipment_engine(args, callback) {
-    shipment_engine(args, callback);
-  }
+  
   transaction_engine(args) {
     transaction_engine(args);
   }
