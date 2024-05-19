3a4c46a1acba555fe0bc69913cbd06cad4fb62b1
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-05-19 12:41:48 -0700

    clean up

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from bleak import BleakClient, BleakError
 
-addresses = ["ab:3b:00:23:de:c5", "xx:xx:xx:xx:xx:xx"]  # Add more as needed
+addresses = ["ab:3b:00:23:de:c5"]  # Add more as needed
 queue = asyncio.Queue()
 
 class ScannerState:
@@ -10,7 +10,6 @@ class ScannerState:
 
 def notification_handler(state):
     def handler(sender, data):
-        """Handler for processing incoming notification data."""
         try:
             barcode = data.decode('utf-8')
             formatted_barcode = barcode.strip().replace("\x00", "")
@@ -28,53 +27,38 @@ async def connect_and_listen(address, state):
     while True:
         client = BleakClient(address)
         try:
-            print(f"Connecting to {address}")
             await client.connect()
-            print(f"Connected to {address}")
             await client.start_notify('00002af0-0000-1000-8000-00805f9b34fb', notification_handler(state))
 
             while client.is_connected:
-                await asyncio.sleep(4)  # Maintain connection checks every second
-
-        except BleakError as e:
-            print(f"BleakError with scanner {address}: {e}")
-        except Exception as e:
-            print(f"Error with scanner {address}: {e}")
+                await asyncio.sleep(4)
+        except BleakError:
+            pass
+        except Exception:
+            pass
         finally:
             if client.is_connected:
                 await client.stop_notify('00002af0-0000-1000-8000-00805f9b34fb')
                 await client.disconnect()
-            print(f"Disconnected from {address}")
 
-        print(f"Reconnecting to {address} in 5 seconds")
         await asyncio.sleep(5)
 
 async def maintain_connection(address):
-    state = ScannerState()  # Create a new state for each scanner
+    state = ScannerState()
     while True:
         await connect_and_listen(address, state)
 
 async def process_queue():
     while True:
         barcode, user_id = await queue.get()
-        print(f"Processed barcode: {barcode}")
-        print(f"User ID: {user_id}")
         queue.task_done()
 
 async def main():
-    # Create queue and all tasks within the same event loop context
     global queue
     queue = asyncio.Queue()
-
-    # Start a task for processing the queue
     processor = asyncio.create_task(process_queue())
-
-    # Start tasks for each scanner
     scanner_tasks = [asyncio.create_task(maintain_connection(address)) for address in addresses]
-
-    # Wait for all tasks to complete (they won't under normal operation)
     await asyncio.gather(processor, *scanner_tasks, return_exceptions=True)
 
-# Run the main function
 if __name__ == "__main__":
     asyncio.run(main())
