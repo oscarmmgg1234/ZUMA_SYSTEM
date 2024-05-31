f5aa2352284b5e7722ee0a48cd9291633c596d3c
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-05-31 11:40:44 -0700

    set up scheduling for notification system

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@ module.exports = {
       name: "NotificationService",
       script: "index.js", // Restart at 2:00 AM every day
       // Other configurations like instances, exec_mode etc.
+      cron_restart: "0 2 * * *",
+      exec_mode: "fork",
+      instances: 1,
     },
   ],
 };
