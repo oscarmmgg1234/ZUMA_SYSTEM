80e0f21980ff70a0a90976fc28f2865fed6bb00a
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-05-31 11:20:49 -0700

    create cluster service for this api

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
-
 module.exports = {
   apps: [
     {
       name: "Inventory API",
       script: "index.js",
-      cron_restart: "0 2 * * *", // Restart at 2:00 AM every day
+      cron_restart: "0 2 * * *",
+      exec_mode: "cluster",
+      instances: 2,
+      // Restart at 2:00 AM every day
       // Other configurations like instances, exec_mode etc.
     },
   ],
