8f2f35ad24fb7f3ede949beb5a6406d0d3180598
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-06-09 21:32:13 -0700

    modified the adding of product

--- Core/Tests/RuntimeTests/tokenTest.js
+++ Core/Tests/RuntimeTests/tokenTest.js
@@ -40,7 +40,7 @@ const insertNewProduct = async (db_handle, args, tokenData) => {
   try {
     // Insert main product
     await db_handle.raw(
-      "INSERT INTO product (PRODUCT_ID, NAME, DESCRIPTION, PRICE, TYPE, LOCATION, COMPANY, ACTIVATION_TOKEN, REDUCTION_TOKEN, SHIPMENT_TOKEN, PROCESS_TYPE, PROCESS_COMPONENT_TYPE, REDUCTION_TYPE, SHIPMENT_TYPE, UNIT_TYPE, MIN_LIMIT, PILL_Ratio, GLYCERIN_RATIO_OZ, Product_Volume, Product_Base_Gallon, MIN_LIMIT_ACTIVE, MIN_ACTIVE_DESC, MIN_STORED_DESC) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+      "INSERT INTO product (PRODUCT_ID, NAME, DESCRIPTION, PRICE, TYPE, LOCATION, COMPANY, ACTIVATION_TOKEN, REDUCTION_TOKEN, SHIPMENT_TOKEN, UNIT_TYPE, MIN_LIMIT, ReferenceStockProduct) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
       [
         args.generatedIDs[0],
         args.name,
@@ -52,26 +52,16 @@ const insertNewProduct = async (db_handle, args, tokenData) => {
         tokenData.activation_token,
         tokenData.reduction_token,
         tokenData.shipment_token,
-        0, // PROCESS_TYPE
-        0, // PROCESS_COMPONENT_TYPE
-        0, // REDUCTION_TYPE
-        0, // SHIPMENT_TYPE
         args.unitType,
         0, // MIN_LIMIT
-        0, // PILL_Ratio
-        0, // GLYCERIN_RATIO_OZ
-        0, // Product_Volume
-        0, // Product_Base_Gallon
-        0, // MIN_LIMIT_ACTIVE
-        "", // MIN_ACTIVE_DESC
-        "", // MIN_STORED_DESC
+        args.RefProduct ? args.RefProduct : "",
       ]
     );
 
     // Insert label product if createLabel is true
     if (args.createLabel === true) {
       await db_handle.raw(
-        "INSERT INTO product (PRODUCT_ID, NAME, DESCRIPTION, PRICE, TYPE, LOCATION, COMPANY, ACTIVATION_TOKEN, REDUCTION_TOKEN, SHIPMENT_TOKEN, PROCESS_TYPE, PROCESS_COMPONENT_TYPE, REDUCTION_TYPE, SHIPMENT_TYPE, UNIT_TYPE, MIN_LIMIT, PILL_Ratio, GLYCERIN_RATIO_OZ, Product_Volume, Product_Base_Gallon, MIN_LIMIT_ACTIVE, MIN_ACTIVE_DESC, MIN_STORED_DESC) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        "INSERT INTO product (PRODUCT_ID, NAME, DESCRIPTION, PRICE, TYPE, LOCATION, COMPANY, ACTIVATION_TOKEN, REDUCTION_TOKEN, SHIPMENT_TOKEN, UNIT_TYPE, MIN_LIMIT, ReferenceStockProduct) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
         [
           args.generatedIDs[1],
           `${args.name} Label`,
@@ -83,19 +73,9 @@ const insertNewProduct = async (db_handle, args, tokenData) => {
           "",
           "",
           `SH:38dh:${args.generatedIDs[1]} UP:235s:${args.generatedIDs[1]}`,
-          0, // PROCESS_TYPE
-          0, // PROCESS_COMPONENT_TYPE
-          0, // REDUCTION_TYPE
-          0, // SHIPMENT_TYPE
           "UNIT",
           0, // MIN_LIMIT
-          0, // PILL_Ratio
-          0, // GLYCERIN_RATIO_OZ
-          0, // Product_Volume
-          0, // Product_Base_Gallon
-          0, // MIN_LIMIT_ACTIVE
-          "", // MIN_ACTIVE_DESC
-          "", // MIN_STORED_DESC
+          args.RefProduct ? args.RefProduct : "",
         ]
       );
     }
