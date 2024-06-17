84bf971d836ecec370ee383e57b063db271cffef
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-06-17 11:12:10 -0700

    new updates to api process

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: "Inventory API",
       script: "index.js",
-      cron_restart: "0 2 * * *",
       exec_mode: "cluster",
       instances: 2,
       // Restart at 2:00 AM every day
