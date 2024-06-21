ec5f5eda4276d0eb807888b5263aa9deb70f5359
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-06-20 17:58:35 -0700

    dependencies install

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-const express_init = require('express')
-const server = express_init()
-const port = 3000
 
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const init_sock_server = require("./Sockets/SystemStatusSocket");
+require("dotenv").config({
+  path:
+    process.env.NODE_ENV === "development"
+      ? ".env.development"
+      : ".env.production",
+});
+
+const middleware = require("./MiddleWare/middleware");
+const activation_endpoints = require("./Routes/Endpoints/activationEndpoints");
+const service_endpoints = require("./Routes/Endpoints/serviceEndpoints");
+const reduction_endpoints = require("./Routes/Endpoints/reductionEndpoints");
+const shipment_endpoints = require("./Routes/Endpoints/shipmentEndpoints");
+const dashboard_endpoints = require("./Routes/Endpoints/dashboardEndpoints");
+const PORT = process.env.PORT || 3001;
+
+server.use(cors());
+server.use(express.json());
+server.use(middleware);
+server.use(activation_endpoints);
+server.use(service_endpoints);
+server.use(reduction_endpoints);
+server.use(shipment_endpoints);
+server.use(dashboard_endpoints);
+
+//init websocket server
+init_sock_server();
+
+server.listen(PORT, () => {
+  console.log(
+    `API is running on port ${PORT}, mode: ${process.env.NODE_ENV}🚀`
+  );
+});
