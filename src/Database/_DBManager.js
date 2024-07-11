7d3498c0c472be5cc769d811119617ddfa91c05a
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-07-10 18:59:25 -0700

    changed db source

--- src/Database/_DBManager.js
+++ src/Database/_DBManager.js
@@ -1,19 +1,18 @@
-const fs = require("fs");
-const tls = require("tls");
-const sslOptions = {
-  ca: fs.readFileSync("../ZUMA_SYSTEM/src/Assets/Certs/DigiCertGlobalRootCA.crt.pem"), // CA certificate
-  rejectUnauthorized: true, // Reject unauthorized connections
-  secureProtocol: "TLSv1_2_method", // Specify the TLS version
-};
+// const fs = require("fs");
+// const tls = require("tls");
+// const sslOptions = {
+//   ca: fs.readFileSync("../ZUMA_SYSTEM/src/Assets/Certs/DigiCertGlobalRootCA.crt.pem"), // CA certificate
+//   rejectUnauthorized: true, // Reject unauthorized connections
+//   secureProtocol: "TLSv1_2_method", // Specify the TLS version
+// };
 exports.query_manager = require("knex")({
-  client: "mysql",
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: sslOptions,
   },
   debug: false,
 });
@@ -24,14 +23,13 @@ console.log(
 );
 
 exports.dev_query_manager = require("knex")({
-  client: "mysql",
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: "zuma_development",
-    ssl: sslOptions,
   },
   debug: false,
 });
