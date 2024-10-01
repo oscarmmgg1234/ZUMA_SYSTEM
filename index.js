55186c46ac2570a6d71fd24cf7ea8f319ae89056
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2024-10-01 15:52:39 -0700

    corrected port config

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const router = require("./ROUTES/endpoints");
 server.use(router);
 
 server.listen(8080, () => {
-  console.log("Server is running on port 4000");
+  console.log("Server is running on port 8080");
 });
