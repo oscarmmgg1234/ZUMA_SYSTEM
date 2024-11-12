5b4d5da91342ee3f076027815d3ea76568af52ea
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2024-11-12 08:14:36 -0800

    updated keys

--- src/NotificationEngine/NotiEngine.js
+++ src/NotificationEngine/NotiEngine.js
@@ -3,24 +3,17 @@ const cron = require("node-cron");
 
 const notification = async () => {
   //Schedule the task to run every weekday (Monday to Friday) at 8:00 AM
-  // cron.schedule(
-  //   "0 8 * * 1-5", // '1-5' specifies Monday to Friday
-  //   async () => {
-  //     await emailGeneration();
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: "America/Los_Angeles", // Replace with your actual timezone
-  //   }
-  // );
-  try{
-  await emailGeneration();
+  cron.schedule(
+    "0 8 * * 1-5", // '1-5' specifies Monday to Friday
+    async () => {
+      await emailGeneration();
+    },
+    {
+      scheduled: true,
+      timezone: "America/Los_Angeles", // Replace with your actual timezone
+    }
+  );
   
-  console.log("email sent");
-  }
-  catch(err){
-    console.log(err);
-  }
 };
 
 exports.notification = notification;
